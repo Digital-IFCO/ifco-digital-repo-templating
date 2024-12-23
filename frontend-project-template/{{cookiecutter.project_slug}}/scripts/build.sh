66bda9414207e91876d592717a55e6e01b8b18a0
javlintor <jlinarestorres2@gmail.com>
2024-12-23 12:52:10 +0100

    remove flake8 test in build script

--- frontend-project-template/{{cookiecutter.project_slug}}/scripts/build.sh
+++ frontend-project-template/{{cookiecutter.project_slug}}/scripts/build.sh
@@ -8,11 +8,3 @@ bash $REPODIR/run_tests.sh || {
   echo "Unit tests failed."
   exit 1
 }
-
-echo "Running Flake8 Tests"
-cd /app/src
-flake8 --output-file="flake8.txt" || {
-  echo "Flake8 tests failed."
-  cat flake8.txt
-  exit 1
-}
