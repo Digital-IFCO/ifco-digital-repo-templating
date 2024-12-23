400e46fa97b8e27e6c1b6d45a28af934309adc81
javlintor <jlinarestorres2@gmail.com>
2024-12-23 12:29:16 +0100

    run test in docker container

--- frontend-project-template/{{cookiecutter.project_slug}}/scripts/run_tests.sh
+++ frontend-project-template/{{cookiecutter.project_slug}}/scripts/run_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 result=0
 coverage erase
-coverage run -m unittest discover /app
+coverage run -m pytest /app/tests/
 if [[ $? -ne 0 ]]; then
-    result=1
+  result=1
 fi
 coverage report -m
 coverage xml -o /app/coverage.xml
 exit $result
+
