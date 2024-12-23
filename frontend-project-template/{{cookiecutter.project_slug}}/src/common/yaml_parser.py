c1eec01b59dd07c8fb3c237bb09032b1a1f7d85f
javlintor <jlinarestorres2@gmail.com>
2024-12-23 13:14:35 +0100

    yaml config

--- frontend-project-template/{{cookiecutter.project_slug}}/src/common/yaml_parser.py
+++ frontend-project-template/{{cookiecutter.project_slug}}/src/common/yaml_parser.py
@@ -4,7 +4,7 @@ import yaml
 class ConfigParser:
     def __init__(self, file_path: Path | str):
         self.file_path = Path(file_path)
-    def parse() -> dict:
-        with open('config.yml', 'r') as file:
+    def parse(self) -> dict:
+        with open(self.file_path, 'r') as file:
             config_parsed = yaml.safe_load(file)
         return config_parsed
